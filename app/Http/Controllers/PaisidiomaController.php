41e0c3da9555ad15ca3830bf16b0e0b5456fe79d
felosc <49010129+felosc@users.noreply.github.com>
2022-07-04 18:42:25 -0500

    creacion modelos y controladores de use, pais, ciudad y pais idioma

---
+++ app/Http/Controllers/PaisidiomaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Paisidioma;
+use Illuminate\Http\Request;
+
+class PaisidiomaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Paisidioma  $paisidioma
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Paisidioma $paisidioma)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Paisidioma  $paisidioma
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Paisidioma $paisidioma)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Paisidioma  $paisidioma
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Paisidioma $paisidioma)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Paisidioma  $paisidioma
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Paisidioma $paisidioma)
+    {
+        //
+    }
+}
---
+++ app/Models/Paisidioma.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Paisidioma extends Model
+{
+    use HasFactory;
+}
