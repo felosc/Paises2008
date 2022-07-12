e0046ecc90ae30a745922b9365defa7613044266
felosc <49010129+felosc@users.noreply.github.com>
2022-07-12 10:32:06 -0500

    se agrego colores a la tabla que muestra la información  en Paises.index

--- resources/views/Paises/index.blade.php
+++ resources/views/Paises/index.blade.php
@@ -1,22 +1,22 @@
 <x-guest-layout>
 
     
-    <table class="table-auto-center">
+    <table class="border-collapse border border-slate-500 ... ">
         <thead>
             <tr>
-                <th class="border border-slate-600 ...">NOMBRE DEL PAIS</th>
-                <th class="border border-slate-600 ...">CODIGO DEL PAIS</th>
-                <th class="border border-slate-600 ...">POBLACION DEL PAIS</th>
-                <th class="border border-slate-600 ...">REGION DEL PAIS</th>
+                <th class="border border-slate-600 ... bg-cyan-100">NOMBRE DEL PAIS</th>
+                <th class="border border-slate-600 ... bg-red-100">CODIGO DEL PAIS</th>
+                <th class="border border-slate-600 ... bg-yellow-100">POBLACION DEL PAIS</th>
+                <th class="border border-slate-600 ... bg-indigo-100">REGION DEL PAIS</th>
             </tr>
         </thead>
         <tbody>
             @foreach ($mostrarpaises as $pais )                    
             <tr>
-                <td class="border border-slate-700 text-center">{{$pais->PaisNombre}}</td>
-                <td class="border border-slate-700 ... text-center">{{$pais->PaisCodigo}}</td>
-                <td class="border border-slate-700 ... text-center">{{$pais->PaisPoblacion}}</td>
-                <td class="border border-slate-700 ... text-center">{{$pais->PaisRegion}}</td>
+                <td class="bg-cyan-200 border border-slate-700 ... text-center">{{$pais->PaisNombre}}</td>
+                <td class="bg-red-200 border border-slate-700 ... text-center">{{$pais->PaisCodigo}}</td>
+                <td class="bg-yellow-200 border border-slate-700 ... text-center">{{$pais->PaisPoblacion}}</td>
+                <td class="bg-indigo-200 border border-slate-700 ... text-center">{{$pais->PaisRegion}}</td>
             </tr>
             @endforeach
     </tbody>
