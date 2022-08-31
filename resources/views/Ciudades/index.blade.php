0810dac1034f2511141bfd4f335d6b1df45f9a5b
felosc <49010129+felosc@users.noreply.github.com>
2022-08-31 10:31:49 -0500

    creacion de la vita de index en ciudades.index

---
+++ resources/views/Ciudades/index.blade.php
@@ -0,0 +1,46 @@
+
+
+
+
+
+        <div class="flex items-center justify-center">     
+          <table id="tablapaises" class="border-separate border border-slate-100 ...">
+            <thead>
+              <tr>
+                  <th class="border border-slate-900 ... bg-slate-200">NOMBRE DE LA CIUDAD</th>
+                  <th class="border border-slate-900 ... bg-slate-200">CODIGO DEL PAIS</th>
+                  <th class="border border-slate-900 ... bg-slate-200">DISTRITO DE LA CIUDAD</th>
+                  <th class="border border-slate-900 ... bg-slate-200">POBLACION DE LA CIUDAD</th>
+                  <th class="border border-slate-900 ... bg-slate-200" colspan="3">Acciones</th>
+                </tr>
+            </thead>
+            <tbody>    
+              @foreach ($mostrarciudades as $ciudad )                    
+              <tr>
+                  <td class="bg-cyan-200 border border-slate-700 ... text-center">{{$ciudad->CiudadNombre}}</td>
+                  <td class="bg-red-200 border border-slate-700 ... text-center">{{$ciudad->PaisCodigo}}</td>
+                  <td class="bg-yellow-200 border border-slate-700 ... text-center">{{$ciudad->CiudadDistrito}}</td>
+                  <td class="bg-indigo-200 border border-slate-700 ... text-center">{{$ciudad->CiudadPoblacion}}</td>                
+                    <td class="bg-indigo-200 border border-slate-700 ... text-center">                   
+                          <a href="{{route('verciudad', $ciudad->id)}}" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-0.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">ver</a>
+                  </td>
+                          <td class="bg-indigo-200 border border-slate-700 ... text-center">
+                            
+                            <a href="{{route('editarciudad', $ciudad->id)}}" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-0.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Editar</a>
+                          </td>
+                          <td class="bg-indigo-200 border border-slate-700 ... text-center">
+                            <form action="{{ route('borrarciudad',$ciudad->id) }}" method="POST">
+                              @csrf
+                              @method('DELETE')
+                              <x-button>Eliminar</x-button>
+                              </form>
+                          </td>                
+              </tr>
+              @endforeach
+      </tbody>
+    </table>
+    </div>
+          <div class="flex items-center justify-center">
+        {{ $mostrarciudades->links() }}
+      </div> 
+    
