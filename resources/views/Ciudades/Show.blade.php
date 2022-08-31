d4caea177d10b2b87abc03a1f586a9011287be48
felosc <49010129+felosc@users.noreply.github.com>
2022-08-31 10:41:18 -0500

    creacion de vista show en ciudades.show

---
+++ resources/views/Ciudades/Show.blade.php
@@ -0,0 +1,32 @@
+
+
+
+
+
+        <div class="bg-yellow-500 col-span-2"> 
+            <label for="" class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">NOMBRE DE LA CIUDAD</label>                   
+            <h5 >{{$verciudad->CiudadNombre}}</h5>
+        </div>
+        <div class="bg-lime-600 col-span-2">
+            <label for="">CODIGO DEL PAIS</label>
+            <p>{{$verciudad->PaisCodigo}}</p>                
+        </div>
+
+        <div class="bg-violet-400">
+            <label for="">DISTRITO DE LA CIUDAD</label>                
+            <p>{{$verciudad->CiudadDistrito}}</p>
+        </div>
+        
+            <div class="bg-violet-400">
+                <label for="">POBLACION DE LA CIUDAD</label>                
+                <p>{{$verciudad->CiudadPoblacion}}</p>
+            </div>
+       
+        </div>
+    </a>
+        
+        <div>
+            <button>
+                <a href="{{route('verciudades')}}" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">home</a>
+            </button>
+        </div>
